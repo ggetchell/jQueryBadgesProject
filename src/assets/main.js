f5676816c897c0faa80c221869f293d9d6311717
Gregory Getchell <ggetchell@gmail.com>
2016-10-29 19:14:17 -0400

    added main.js updates

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
-
+$.ajax({
+    url: https://www.codeschool.com/users/1035153.json
+    dataType: 'jsonp',
+    success: function(response) {
+    
+}
+})
   // your code will go here
 
 });
