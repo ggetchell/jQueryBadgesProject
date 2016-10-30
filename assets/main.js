e7f655c92861714ad41c28da376a78169ce2e956
Gregory Getchell <ggetchell@gmail.com>
2016-10-29 20:01:19 -0400

    Completed Project

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,41 @@
 $(function() {
-$.ajax({
-    url: https://www.codeschool.com/users/1035153.json
+
+    $.ajax({
+    url: 'https://www.codeschool.com/users/1035153.json',
     dataType: 'jsonp',
     success: function(response) {
-    
+    addCompCourses(response.courses.completed);
 }
 })
-  // your code will go here
-
+  
+    function addCompCourses(courses) {
+        
+        var $badges = $('#badges');
+        
+        courses.forEach(function(course) {
+            
+           // console.log(course);
+            
+          var $course = $('<div />', {
+                'class': 'course'
+        }).appendTo($badges);
+        
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+           
+        $('<img />', {
+            src: course.badge
+        }).appendTo($course);
+            
+        $('<a />', {
+            'class': 'btn btn-primary',
+            target: '_blank',
+            href: course.url,
+            text: 'View Course'
+        }).appendTo($course);    
+            
+      }) 
+      
+    }
 });
